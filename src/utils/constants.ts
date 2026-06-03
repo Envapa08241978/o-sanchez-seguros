50010ffa81216b9b12a908bf7484e86cacdc473c
Enrique Vale <enriquevale.ev@gmail.com>
2026-06-02 19:04:15 -0700

    feat(contact): add google maps iframe and update exact address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ export const SITE_CONFIG = {
   whatsapp: "526621822481",
   whatsappUrl: "https://wa.me/526621822481",
   email: "oscareduardosanchezaguirre@gmail.com",
-  address: "Hermosillo, Sonora, México",
+  address: "Blvd. Juan Navarrete 154-Int. 168U, Valle Grande, 83205 Hermosillo, Son.",
   googleMapsUrl: "https://www.google.com/maps/place/OSanchez+Seguros+-+Oficina+Oscar+Sanchez/@29.0862245,-110.9775244,950m/data=!3m2!1e3!4b1!4m6!3m5!1s0x86ce857f14777283:0x9925c26431d0ec84!8m2!3d29.0862198!4d-110.9749495",
   yearFounded: 2009,
   yearsOfExperience: 16,
